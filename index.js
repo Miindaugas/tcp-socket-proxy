2c61a6a51afe208c9ea0dd1ae7285307a11a1158
Mindaugas Dangveckis <m.dangveckis@gmail.com>
2020-02-18 10:53:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,63 +3,32 @@ const whitelist = process.env.PROXY_WHITELIST;
 const localport = process.env.PROXY_LOCAL_PORT;
 const remoteport = process.env.PROXY_REMOTE_PORT;
 const remotehost = process.env.PROXY_REMOTE_HOST;
-const socketIO = require('socket.io');
-const express = require('express');
-
-const PORT = process.env.PORT || 3000;
-const INDEX = '/index.html';
-
-const server = express()
-    .listen(3000, () => console.log(`Listening on ${PORT}`));
-
-const io = socketIO(server);
-
-io.on('connection', (socket) => {
-    console.log('Client connected');
-    socket.write("wtf");
-    socket.flush();
-    socket.close();
-    socket.on('disconnect', () => console.log('Client disconnected'));
-});
 
 console.log(`Proxy: ${whitelist}:${localport} -> ${remotehost}:${remoteport}`);
 
-
-//
-// net.createServer((local) => {
-//     console.log('Connected!', local.remoteAddress);
-//
-//         // White list only your connection
-//     if (local.remoteAddress !== whitelist) {
-//         local.end();
-//         return;
-//     }
-//
-//     console.log('Creating tunnel');
-//
-//     const redirect = (source, destination) => {
-//         source.on('data', (data) => {
-//             const flushed = remote.write(data);
-//             if (!flushed) {
-//                 source.pause();
-//             }
-//         });
-//         source.on('drain', () => {
-//             destination.resume();
-//         });
-//         source.on('close', () => {
-//             destination.end();
-//         });
-//     };
-//
-//     // Redirect local connections to remote & remote to local
-//     const remote = new net.Socket();
-//     redirect(local, remote);
-//     redirect(remote, local);
-//     remote.connect(remoteport, remotehost, () => {
-//         console.log('Connected to target!');
-//     });
-//
-// }).listen(3000);
-
-console.log(process.env.PORT || 80);
+net.createServer((local) => {
+    // White list only your connection
+    if (local.remoteAddress !== whitelist) {
+        local.end();
+        return;
+    }
+    const redirect = (source, destination) => {
+        source.on('data', (data) => {
+            const flushed = remote.write(data);
+            if (!flushed) {
+                source.pause();
+            }
+        });
+        source.on('drain', () => {
+            destination.resume();
+        });
+        source.on('close', () => {
+            destination.end();
+        });
+    };
+    // Redirect local connections to remote & remote to local
+    const remote = new net.Socket();
+    redirect(local, remote);
+    redirect(remote, local);
+    remote.connect(remoteport, remotehost);
+}).listen(localport, '0.0.0.0');
